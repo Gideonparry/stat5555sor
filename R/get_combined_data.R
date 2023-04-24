7dad4f8ee8d6ef9c35195ea68512be12f5af8595
Gideonparry <gideonparry@gmail.com>
2023-04-24 15:35:51 -0600

    added some comments

--- R/get_combined_data.R
+++ R/get_combined_data.R
@@ -13,13 +13,19 @@ get_combined_data <- function(year) {
   kenpom_data <- fetch_and_format_kenpom_data(year)
   warrennolan_data <- fetch_and_format_warrennolan_data(year)
   combined_data <- combine_datasets(kenpom_data, warrennolan_data)
+
+  ## Complete cases drops non d1 opponents
   combined_data <- combined_data[complete.cases(combined_data), ]
+
+  ## adding home_sor
   combined_data <- combined_data |>
     dplyr::rowwise() |>
     dplyr::mutate(home_sor = sor_points(
       Home_score, Away_score, away_NET,
       nrow(warrennolan_data), TRUE, neutral
     ))
+
+  ## adding away_sor
   combined_data <- combined_data |>
     dplyr::rowwise() |>
     dplyr::mutate(away_sor = sor_points(
